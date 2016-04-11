42626b120081de0fb1670f29978735b61deb9d91
Vadim Sinitski <rusboard@gmail.com>
2016-04-11 23:06:00 +0300

    Sunday evening changes

--- vadim/lesson_1_todo_list/main.jsx
+++ vadim/lesson_1_todo_list/main.jsx
@@ -1,3 +1,8 @@
+'use strict';
+
+var ESCAPE_KEY = 27;
+var ENTER_KEY = 13;
+
 var tasks = [
     {id: 1, status: 0, name: 'Finish ToDo App'},
     {id: 2, status: 0, name: 'Go to lunch'},
@@ -16,12 +21,29 @@ var TodoApp = React.createClass({
 });
 
 var TodoAddTask = React.createClass({
+    handleKeyDown: function (event) {
+        if (event.which === ESCAPE_KEY) {
+            console.log('TodoAddTask: Escape button detected in add task text field');
+        } else if (event.which === ENTER_KEY) {
+            console.log('TodoAddTask: Enter button detected in add task text field');
+            this.addTaskAction();
+        }
+    },
+    addTaskAction: function() {
+        console.log('TodoAddTask: addTaskAction action');
+    },
     render: function() {
         return (
             <div>
                 <h1>Add task</h1>
                 <div>
-                    <input type="text" /><button>add</button>
+                    <input
+                        type="text"
+                        onKeyDown={this.handleKeyDown}
+                        onBlur={this.addTaskAction}
+                    />
+
+                    <button onClick={this.addTaskAction}>add</button>
                 </div>
             </div>
         );
@@ -39,9 +61,32 @@ var TodoTaskList = React.createClass({
 });
 
 var TodoTaskLine = React.createClass({
+    handleKeyDown: function (event) {
+        if (event.which === ESCAPE_KEY) {
+            console.log('TodoTaskLine: Escape button detected in add task text field');
+        } else if (event.which === ENTER_KEY) {
+            console.log('TodoTaskLine: Enter button detected in add task text field');
+        }
+    },
+    handleEdit: function (event) {
+        console.log('TodoTaskLine: Double click event');
+    },
     render: function() {
         return (
-            <li key="{this.props.id}"><input type="checkbox" /><span>{this.props.name}</span></li>
+            <li key="{this.props.id}">
+                <input
+                    type="checkbox"
+                    checked=""
+                    onChange=""
+                />
+
+                <span onDoubleClick={this.handleEdit}>{this.props.name}</span>
+
+                <input
+                    value={this.props.name}
+                    onKeyDown={this.handleKeyDown}
+                />
+            </li>
         );
     }
 });
